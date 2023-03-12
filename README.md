dbb26e239d235e585ace7a96281c06c60c79c560
blacknick <906991067@qq.com>
2023-03-12 20:12:25 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# MicroKV
+MicroKV is a tiny kv engine.
+## build
+The lib is built using cmake. You can run run.sh to build this repo on Linux.
+```
+chmod +x ./run.sh
+./run.sh
+```
+## use
+TODO
---
+++ include/SkipListNode.hpp
@@ -0,0 +1,29 @@
+#include <cstddef>
+namespace microkv {
+
+template<class K, class V>
+class SkipListNode {
+public:
+    SkipListNode();
+    SkipListNode(K _key, V _value, size_t _level, SkipListNode *next = nullptr);
+    SkipListNode<K, V> **forward;
+// private:
+    K key;
+    V value;
+    size_t level;
+};
+
+template<class K, class V>
+SkipListNode<K, V>::SkipListNode() : key{0}, value{0}, level(0)  {
+
+}
+
+template<class K, class V>
+SkipListNode<K, V>::SkipListNode(K _key, V _value, size_t _level, SkipListNode* next) : key(_key), value(_value), level(_level) {
+    forward = new SkipListNode*[level + 1];
+    for (size_t i = 0; i <= level; ++i) {
+        forward[i] = next;
+    }
+}
+
+}
