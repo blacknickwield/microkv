a3aef7c31eb587dbdf61fce141891a2dd24261b9
blacknick <906991067@qq.com>
2023-03-17 21:26:27 +0800

    [refactor] hide encode call

--- include/util/Encoder.hpp
+++ include/util/Encoder.hpp
@@ -8,8 +8,4 @@ void Put16Bit(std::string &dst, uint16_t val);
 void Put32Bit(std::string &dst, uint32_t val);
 void Put64Bit(std::string &dst, uint64_t val);
 
-void Encode8Bit(char *dst, uint8_t val);
-void Encode16Bit(char *dst, uint16_t val);
-void Encode32Bit(char *dst, uint32_t val);
-void Encode64Bit(char *dst, uint64_t val);
 } // namespace microkv
