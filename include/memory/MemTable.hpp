a29fb4e81a23a8bb7824e40725c9a037a470e153
blacknick <906991067@qq.com>
2023-03-21 16:46:52 +0800

    [feat] implement basic MemTable

--- include/memory/MemTable.hpp
+++ include/memory/MemTable.hpp
@@ -6,14 +6,21 @@
 #include "Allocator.hpp"
 
 namespace microkv {
+class MemTableIterator;
+
 class MemTable {
 public:
     explicit MemTable(std::shared_ptr<Allocator> allocator_);
     MemTable() = delete;
+public:
+    auto Add(const std::string &key, const std::string &value) -> bool;
+    auto Get(const std::string &key, std::string &value) const -> bool;
+    auto Update(const std::string &key, const std::string &value) -> bool;
+    auto Delete(const std::string &key) -> bool;
 private:
-    // std::shared_ptr<SkipList<>> m_table;
-    std::shared_ptr<SkipList<std::string, std::string>> table;
+    std::shared_ptr<SkipList<std::string>> table;
     std::shared_ptr<Allocator> allocator;
+    friend class MemTableIterator;
 };
 
 } // namespace microk 
--- src/memory/MemTable.cpp
+++ src/memory/MemTable.cpp
@@ -3,7 +3,31 @@
 namespace microkv {
 
 MemTable::MemTable(std::shared_ptr<Allocator> allocator_) : allocator(allocator_) {
+    table = std::make_shared<SkipList<std::string>>(allocator);
 
+}
+
+auto MemTable::Add(const std::string &key, const std::string &value) -> bool {
+    return table->Insert(key, value);
+}
+
+
+
+auto MemTable::Get(const std::string &key, std::string &value) const -> bool {
+    return table->Find(key, value);
+}
+
+auto MemTable::Update(const std::string &key, const std::string &value) -> bool {
+    table->Erase(key);
+    return table->Insert(key, value);
+}
+
+
+auto MemTable::Delete(const std::string &key) -> bool {
+    if (table->Count(key)) {
+        return table->Erase(key);
+    }
+    return false;
 }
 
 }
